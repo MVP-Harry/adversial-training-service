c2896fd1e81b6ba96bc1c16eb6dbcd455c77bbb9
floatingtrees <96200235+floatingtrees@users.noreply.github.com>
2024-02-17 10:24:25 -0800

    added losses

--- FGSM.py
+++ FGSM.py
@@ -7,6 +7,7 @@ def FGSM_attack(model, dataloader, total_batches, from_logits = True, loss_fn = 
 	model.eval()
 	percent_change = 0
 	count = 0
+	running_loss = 0
 	for batch in dataloader:
 		x, y = batch
 		data = x.clone().detach()
@@ -16,21 +17,28 @@ def FGSM_attack(model, dataloader, total_batches, from_logits = True, loss_fn = 
 		if from_logits:
 			outputs = torch.nn.functional.softmax(outputs, dim = 1)
 		loss = loss_fn(outputs, y)
-		print(y.size())
 		loss.backward()
 		data_grad = data.grad.data
 		perturbed_images = data + epsilon * data_grad.sign()
 		generated_images.append(perturbed_images)
+
 		original_labels.append(y)
 		count += 1
+
+		new_preds = model(perturbed_images)
+		new_loss = loss_fn(new_preds, y)
+		loss_change = new_loss.item() - loss.item()
+		running_loss += loss_change
 		if count >= total_batches:
 			break
-	return generated_images, original_labels
+	running_loss /= count
+	return generated_images, original_labels, running_loss
 
 
 def PGD_attack(model, dataloader, total_batches, iterations, from_logits = True, loss_fn = torch.nn.CrossEntropyLoss(), epsilon = 0.001):
 	count = 0
-	percent_change = 0
+	running_loss = 0
+
 	generated_images = []
 	original_labels = []
 	for batch in dataloader:
@@ -48,27 +56,39 @@ def PGD_attack(model, dataloader, total_batches, iterations, from_logits = True,
 			data_grad = data.grad.data
 			perturbed_images = data + epsilon * data_grad
 		count += 1
+
+		new_preds = model(perturbed_images)
+		new_loss = loss_fn(new_preds, y)
+		loss_change = new_loss.item() - loss.item()
+		running_loss += loss_change
+
 		generated_images.append(perturbed_images)
 		original_labels.append(y)
+
 		if count >= total_batches:
 			break
-	return generated_images, original_labels
+
+	running_loss /= count
+
+	return generated_images, original_labels, running_loss
 
 def targeted_adversarial_attack(model, dataloader, total_batches, iterations, target_label, from_logits = True, loss_fn = torch.nn.CrossEntropyLoss()):
 	count = 0
 	percent_change = 0
 	generated_images = []
 	original_labels = []
+	running_loss = 0
 	for batch in dataloader:
 		x, y = batch
 		data = x.clone().detach()
 		data.requires_grad = True
 		original_preds = model(data)
+		optimizer = torch.optim.Adam(list([data, ]), maximize = False)
 
 		for i in range(iterations):
-			
+			optimizer.zero_grad()
 			outputs = model(data)
-			optimizer = torch.optim.Adam(list([data, ]), maximize = False)
+			
 			if from_logits:
 				outputs = torch.nn.functional.softmax(outputs, dim = 1)
 			labels = target_label
@@ -76,13 +96,16 @@ def targeted_adversarial_attack(model, dataloader, total_batches, iterations, ta
 			loss.backward()
 			optimizer.step()
 		new_preds = model(data)
+		new_loss = loss_fn(new_preds, labels)
+		running_loss = new_loss.item() - loss.item()
 		count += 1
 		generated_images.append(data)
 		original_labels.append(y)
 		if count >= total_batches:
 			break
 
-	return generated_images, original_labels
+
+	return generated_images, original_labels, running_loss
 
 
 
--- server.py
+++ server.py
@@ -1,39 +1,49 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import torch
+import FGSM as attacks
 import pickle
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # Gets the size of data
-        post_data = self.rfile.read(content_length) # Gets the data itself
-        try:
-            data = pick.loads(post_data.decode('utf-8'))
-            dataloader = data.get('dataloader')
-            model = data.get('model')
-            options = data.get('options')
-            self.send_response(200)
-            self.send_header('Content-type', 'application/pickle')
-            self.end_headers()
-            self.wfile.write(response_json.encode('utf-8'))
-            # Check if 'number' is a valid integer
-            d1 = FGSM_attack(model, dataloader)
-            response_dict = {'dataset': dataset}
-            response_json = pickle.dumps(response_dict)
-                
-                
-            else:
-                self.send_error(400, "Invalid input. Please send a JSON with an integer 'number'.")
-        except json.JSONDecodeError:
-            self.send_error(400, "Invalid JSON format. Please send a valid JSON.")
+	def do_POST(self):
+		content_length = int(self.headers['Content-Length']) # Gets the size of data
+		post_data = self.rfile.read(content_length) # Gets the data itself
+
+		data = pickle.loads(post_data.decode('utf-8'))
+		dataloader = data.get('dataloader')
+		model = data.get('model')
+		options = data.get('options')
+		imagenet = options["imagenet"]
+		response = dict()
+		
+		
+		# Check if 'number' is a valid integer
+		if options["FGSM"]:
+			d1, prev_labels = attacks.FGSM_attack(model, dataloader, options["FGSM_batches"]) # returns list, list
+			response["FGSM data"] = pickle.dumps(d1)
+			response["FGSM labels"] = pickle.dumps(prev_labels)
+			response["FGSM image"] = 
+		if options["PGD"]:
+			d2, prev_labels = attacks.PGD_attack(model, dataloader, options["FGSM_batches"], options['PGD_iterations'])
+		if options["targeted"]:
+			d3, prev_labels = attacks.targeted_adversarial_attack(model, dataloader, options["targeted_batches"], 
+				options["targeted_iterations"], options["targeted_label"])
+
+		response_dict = {'dataset': dataset}
+		self.send_response(200)
+		self.send_header('Content-type', 'application/pickle')
+		self.end_headers()
+		reponse = pickle.dumps(response_dict)
+		self.wfile.write(response.encode('utf-8'))
+			
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, addr="localhost", port=8000):
-    server_address = (addr, port)
-    httpd = server_class(server_address, handler_class)
+	server_address = (addr, port)
+	httpd = server_class(server_address, handler_class)
 
-    print(f"Starting http server on {addr}:{port}")
-    httpd.serve_forever()
+	print(f"Starting http server on {addr}:{port}")
+	httpd.serve_forever()
 
 if __name__ == "__main__":
-    run()
+	run()
